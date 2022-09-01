1511bc726dfecd46be820221f1b77ac6b65a97a4
Chathura Deshapriya <57343967+chathuradeshapriya@users.noreply.github.com>
2022-09-01 14:06:23 +0530

    Auto scroll horizotal

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -12,33 +12,43 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   ScrollController _scrollController1 = ScrollController();
+  ScrollController _scrollController2 = ScrollController();
+  ScrollController _scrollController3 = ScrollController();
 
- void initState() {
-   super.initState();
-   WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-     double minScrollExtent1 = _scrollController1.position.minScrollExtent;
-     double maxScrollExtent1 = _scrollController1.position.maxScrollExtent;
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      double minScrollExtent1 = _scrollController1.position.minScrollExtent;
+      double maxScrollExtent1 = _scrollController1.position.maxScrollExtent;
 
-     animateToMaxMin(maxScrollExtent1, minScrollExtent1, maxScrollExtent1, 25,
-     _scrollController1);
+      double minScrollExtent2 = _scrollController1.position.minScrollExtent;
+      double maxScrollExtent2 = _scrollController1.position.maxScrollExtent;
 
+      double minScrollExtent3 = _scrollController1.position.minScrollExtent;
+      double maxScrollExtent3 = _scrollController1.position.maxScrollExtent;
 
-   });
+      animateToMaxMin(maxScrollExtent1, minScrollExtent1, maxScrollExtent1, 25,
+          _scrollController1);
 
+      animateToMaxMin(maxScrollExtent2, minScrollExtent2, maxScrollExtent2, 25,
+          _scrollController2);
 
- }
-
+      animateToMaxMin(maxScrollExtent3, minScrollExtent3, maxScrollExtent3, 25,
+          _scrollController3);
+    });
+  }
 
   animateToMaxMin(double max, double min, double direction, int seconds,
       ScrollController scrollController) {
     scrollController
         .animateTo(direction,
-        duration: Duration(seconds: seconds), curve: Curves.linear)
+            duration: Duration(seconds: seconds), curve: Curves.linear)
         .then((value) {
       direction = direction == max ? min : max;
       animateToMaxMin(max, min, direction, seconds, scrollController);
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -49,7 +59,13 @@ class _HomeScreenState extends State<HomeScreen> {
             MoviesListView(
               scrollController: _scrollController1,
               images: movies1,
-            )
+            ),            MoviesListView(
+              scrollController: _scrollController2,
+              images: movies2,
+            ),            MoviesListView(
+              scrollController: _scrollController3,
+              images: movies3,
+            ),
           ],
         ),
       ),
